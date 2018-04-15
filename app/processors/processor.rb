40fcd421fc107f1a1295de7bea28eca67d774722
Maciej Głowacki <maciej.glowacki@daftcode.pl>
2018-04-15 14:46:20 +0200

    Add formatters documentation

--- app/processors/processor.rb
+++ app/processors/processor.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# This class uses a +formatter+ to format +text+ and return it
+# as a hash containing both original and processed text.
+#
+# Example:
+#   class Upcaser
+#     def self.format(text)
+#       text.to_s.upcase
+#     end
+#   end
+#
+#   processor = Processor.new(text: 'Foo', formatter: UpcaseFormatter.new)
+#   processor.format # => { original: { data: 'Foo' }, message: 'FOO' }
 class Processor
   def initialize(text:, formatter:)
     @text = text.to_s
--- app/processors/reverser.rb
+++ app/processors/reverser.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# This class is a text formatter which returns reversed string
+#
+# Example:
+#   Reverser.format('Foo') # => 'ooF'
 class Reverser
   def self.format(text)
     text.to_s.reverse
